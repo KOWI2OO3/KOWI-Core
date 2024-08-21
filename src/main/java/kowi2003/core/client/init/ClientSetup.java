17edebfb72b6f4c020f5f64588102e4641e861f3
KOWI2003 <2003kowi@gmail.com>
2024-08-21 15:20:21 +0200

    Properly split client from common

--- src/main/java/kowi2003/core/client/init/ClientSetup.java
+++ src/main/java/kowi2003/core/client/init/ClientSetup.java
@@ -1,8 +1,14 @@
 package kowi2003.core.client.init;
 
+import kowi2003.core.Core;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = Core.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public final class ClientSetup {
     
-    public static void onClientSetup() 
+    public static void onClientSetup(final FMLClientSetupEvent event) 
     {
 
     }
