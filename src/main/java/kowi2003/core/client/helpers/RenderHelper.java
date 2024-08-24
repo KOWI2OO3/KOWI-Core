0177cd5a521b5d977568baeb38699bfc22f8c69f
KOWI2003 <2003kowi@gmail.com>
2024-08-24 20:05:56 +0200

    Added basic render helper

---
+++ src/main/java/kowi2003/core/client/helpers/RenderHelper.java
@@ -0,0 +1,22 @@
+package kowi2003.core.client.helpers;
+
+import net.minecraft.client.renderer.texture.OverlayTexture;
+
+/**
+ * A helper class for rendering, from simple to complex rendering tasks
+ * 
+ * @author KOWI2003
+ */
+public final class RenderHelper 
+{   
+    /**
+     * The full brightness value. Can be used as a standin for combined light
+     */
+    public static final int FULL_BRIGHTNESS = 0xFFFFFF;
+
+    /**
+     * The no overlay value. Can be used as a standin for combined overlay
+     */
+    public static final int NO_OVERLAY = OverlayTexture.NO_OVERLAY;
+
+}
