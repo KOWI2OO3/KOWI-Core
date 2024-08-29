1c15867ca826b48e4080f78d9cb8760e13171d61
KOWI2003 <2003kowi@gmail.com>
2024-08-29 21:55:40 +0200

    Removed useless register method

--- src/main/java/kowi2003/core/common/registries/BlockRegister.java
+++ src/main/java/kowi2003/core/common/registries/BlockRegister.java
@@ -3,11 +3,7 @@ package kowi2003.core.common.registries;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import javax.annotation.Nullable;
-
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -36,31 +32,18 @@ public class BlockRegister implements IRegistry {
      * @return the registry object for this block
      */
     public <T extends Block> RegistryObject<T> register(String id, final Supplier<? extends T> block) {
-        return register(id, null, block);
+        return register(id, block, b -> new BlockItem(b, new Item.Properties()));
     }
 
     /**
      * register and adds the block them to the specified creative tab
      * @param <T> the block type
      * @param id the id of the item, note only lower case letters and dots and dasses '_' allowed
-     * @param tab the tab to add the item to, null to not add it to a creative tab
-     * @param block the block to add as a supplier
-     * @return the registry object for this block
-     */
-    public <T extends Block> RegistryObject<T> register(String id, final @Nullable Supplier<ResourceKey<CreativeModeTab>> tab, final Supplier<? extends T> block) {
-        return register(id, tab, block, b -> new BlockItem(b, new Item.Properties()));
-    }
-
-    /**
-     * register and adds the block them to the specified creative tab
-     * @param <T> the block type
-     * @param id the id of the item, note only lower case letters and dots and dasses '_' allowed
-     * @param tab the tab to add the item to, null to not add it to a creative tab
      * @param block the block to add as a supplier
      * @param blockItemFunction the function to use to get the item from the block
      * @return the registry object for this block
      */
-    public <T extends Block> RegistryObject<T> register(String id, final @Nullable Supplier<ResourceKey<CreativeModeTab>> tab, final Supplier<? extends T> block, Function<T, BlockItem> blockItemFunction)
+    public <T extends Block> RegistryObject<T> register(String id, final Supplier<? extends T> block, Function<T, BlockItem> blockItemFunction)
     {
         RegistryObject<T> result = BLOCK_REGISTRY.register(id, block);
         ITEM_REGISTER.register(id, () -> blockItemFunction.apply(result.get()));
