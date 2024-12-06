be8b6723fa229777729a625e1d06ac37a72aa12e
KOWI2003 <2003kowi@gmail.com>
2024-12-06 20:13:17 +0100

    Added default item type

--- src/main/java/kowi2003/core/common/items/interfaces/ICapabilityItem.java
+++ src/main/java/kowi2003/core/common/items/interfaces/ICapabilityItem.java
@@ -3,8 +3,22 @@ package kowi2003.core.common.items.interfaces;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 
+/**
+ * This allows an item to define its own capabilities by forwarding the event to the item class.
+ * this is an easy and straight forward way to add capabilities to your items.
+ * 
+ * The core handles all of the calling and event handling and you can define your own capabilities for your item
+ * 
+ * @author KOWI2003
+ */
 public interface ICapabilityItem {
 
+	 /**
+     * This method is called on the event of attaching capabilities onto your item. Its a forwarded event which means you should add the capability yourself.
+     * adding the capability can be done using the event and the @see{kowi2003.core.capability.item.ItemCapabilityProvider ItemCapabilityProvider} of the core or a custom capability provider 
+     * @param stack the item stack to attach the capability to
+     * @param event the event that attaches the capabilities
+     */
 	void getherCapabilities(ItemStack stack, AttachCapabilitiesEvent<ItemStack> event);
 	
 }
